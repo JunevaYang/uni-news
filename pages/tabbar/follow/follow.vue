1b27277aff0ad38ec9e95158df392b1ddf675db5
junevaYang <353196481@qq.com>
2021-08-10 16:58:07 +0800

    init

---
+++ pages/tabbar/follow/follow.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		guanzhu
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/tabbar/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+	<view class="content">
+		<!-- 自定义导航栏 -->
+		<navbar></navbar>
+		<view v-for="(item, index) in 100" :key="index">
+			{{item}} neir
+		</view>
+	</view>
+</template>
+
+<script>
+	//easyCom
+	//只要组件目录是  components/组件名/组件名.vue 就可以直接使用不需要引用 局部引入
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	
+</style>
